cfb4e885beac770eed58c704338fd9cc62d0f226
ajilesh p p <ajileshpp2@gmail.com>
2022-12-09 14:33:21 +0400

    Auth created in project

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function Login(REQUEST $request)
+    {
+        
+    }
+}
